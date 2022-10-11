62cbd7e4f1fca2d06a07c7ce8f17b6f93759908c
XantX <hp_xantx@hotmail.com>
2022-10-11 14:00:51 -0500

    First Commit Description: First commit with initial configuration TODO: Config axon framework

---
+++ src/main/java/com/meetyourroommate/app/MeetyourroommateApplication.java
@@ -0,0 +1,13 @@
+package com.meetyourroommate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetyourroommateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetyourroommateApplication.class, args);
+	}
+
+}
