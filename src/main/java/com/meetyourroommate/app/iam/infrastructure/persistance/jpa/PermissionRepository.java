fac4a9b7899842a5672af9bfb3658642ced3a63a
renato-17 <renato.avela.17@gmail.com>
2022-10-19 18:35:25 -0500

    feat[iam]: Repository definition done

--- src/main/java/com/meetyourroommate/app/iam/infrastructure/persistance/jpa/PermissionRepository.java
+++ src/main/java/com/meetyourroommate/app/iam/infrastructure/persistance/jpa/PermissionRepository.java
@@ -1,9 +1,13 @@
 package com.meetyourroommate.app.iam.infrastructure.persistance.jpa;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.meetyourroommate.app.iam.domain.entities.Permission;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
-  Optional<Permission> findByName(PermissionName name);
+  Optional<Permission> findByName(String name);
 }
-  
--- src/main/java/com/meetyourroommate/app/iam/infrastructure/persistance/jpa/RoleRepository.java
+++ src/main/java/com/meetyourroommate/app/iam/infrastructure/persistance/jpa/RoleRepository.java
@@ -1,5 +1,14 @@
 package com.meetyourroommate.app.iam.infrastructure.persistance.jpa;
 
-public class RoleRepository {
-  
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.meetyourroommate.app.iam.domain.entities.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(String name);
 }
+
