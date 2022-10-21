b8da842055312bca16b6884d6182c3e4c150d615
XantX <hp_xantx@hotmail.com>
2022-10-21 10:55:28 -0500

    <fix>[profile]:fix profile create by user id

--- src/main/java/com/meetyourroommate/app/profile/interfaces/rest/ProfileController.java
+++ src/main/java/com/meetyourroommate/app/profile/interfaces/rest/ProfileController.java
@@ -1,31 +1,44 @@
 package com.meetyourroommate.app.profile.interfaces.rest;
 
+import com.meetyourroommate.app.iam.application.services.UserService;
+import com.meetyourroommate.app.iam.domain.aggregates.Users;
 import com.meetyourroommate.app.profile.application.services.ProfileService;
 import com.meetyourroommate.app.profile.application.transform.ProfileMapper;
 import com.meetyourroommate.app.profile.application.transform.resources.ProfileResource;
 import com.meetyourroommate.app.profile.domain.aggregates.Profile;
+import org.h2.engine.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1/profiles")
 public class ProfileController {
     private ProfileService profileService;
+    private UserService userService;
     private ProfileMapper mapper;
 
-    public ProfileController(ProfileService profileService, ProfileMapper mapper) {
+    public ProfileController(ProfileService profileService, UserService userService, ProfileMapper mapper) {
         this.profileService = profileService;
+        this.userService = userService;
         this.mapper = mapper;
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> save(@RequestParam String UserUuid, @RequestBody ProfileResource profileResource){
+    public ResponseEntity<?> save(@RequestParam String userId, @RequestBody ProfileResource profileResource){
         try
         {
-            Profile profile = mapper.toEntity(profileResource);
-            return new ResponseEntity<Profile>(profile, HttpStatus.OK);
+            Optional<Users> user = userService.findById(userId);
+            if(user.isPresent()){
+                Profile profile = mapper.toEntity(profileResource);
+                profile.setUser(user.get());
+                return new ResponseEntity<Profile>(profileService.save(profile), HttpStatus.OK);
+            }else{
+               return new ResponseEntity<String>("User not found.", HttpStatus.NOT_FOUND);
+            }
         }catch(Exception e){
            return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
