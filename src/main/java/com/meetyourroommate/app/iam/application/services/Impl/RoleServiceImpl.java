faa899a591e63b2a5e5f15dced99b0a38c88f1b5
XantX <hp_xantx@hotmail.com>
2022-10-22 17:10:19 -0500

    <feat>[roles]: added role creation

---
+++ src/main/java/com/meetyourroommate/app/iam/application/services/Impl/RoleServiceImpl.java
@@ -0,0 +1,48 @@
+package com.meetyourroommate.app.iam.application.services.Impl;
+
+import com.meetyourroommate.app.iam.application.services.RoleService;
+import com.meetyourroommate.app.iam.domain.entities.Role;
+import com.meetyourroommate.app.iam.infrastructure.persistance.jpa.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role save(Role role) throws Exception {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public List<Role> findAll() throws Exception {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> findById(Long id) throws Exception {
+        return roleRepository.findById(id);
+    }
+
+    @Override
+    public Role update(Long aLong, Role role) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) throws Exception {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Role> findByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
--- src/main/java/com/meetyourroommate/app/iam/domain/aggregates/User.java
+++ src/main/java/com/meetyourroommate/app/iam/domain/aggregates/User.java
@@ -2,16 +2,16 @@ package com.meetyourroommate.app.iam.domain.aggregates;
 
 import javax.persistence.*;
 
-import com.meetyourroommate.app.profile.domain.aggregates.Profile;
+import com.meetyourroommate.app.iam.domain.entities.Role;
 import com.meetyourroommate.app.shared.domain.valueobjects.Audit;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateRoot;
-import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
 import com.meetyourroommate.app.iam.domain.valueobjects.Email;
 
 @AggregateRoot
 @Entity
+@Table(name = "users")
 public class User {
   @Id
   @AggregateIdentifier
@@ -22,6 +22,10 @@ public class User {
 
   @Embedded
   private Audit audit;
+
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
 
   public User(){
     this.active = true;
@@ -60,39 +64,4 @@ public class User {
     this.audit = audit;
   }
 
-  /*  @ManyToMany
-  @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-  @JsonIgnore
-  private Set<Role> roles;
-
-  @ManyToMany
-  @JoinTable(name = "user_permission", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
-  @JsonIgnore
-  private Set<Permission> permissions;
-
-  @Autowired
-  @JsonIgnore
-  private UserService userService;
-  @CommandHandler
-  public void handle(SignInUserCommand command) {
-
-    Optional<User> userOptional = userRepository.findByEmail(command.getEmail());
-
-    if (!userOptional.isPresent()) {
-      throw new ResourceValidationException("User with email: " + command.getEmail() + " not found");
-    }
-
-    User user = userOptional.get();
-    if (user.getPassword().confirmPassword(command.getPassword())) {
-      apply(new UserSignedInEvent(user.getId(), user.getEmail(), user.getPassword()));
-    }
-  }
-
-  @EventSourcingHandler
-  public void on(UserSignedInEvent event) {
-    this.id = event.getId();
-    this.email = event.getEmail();
-    this.password = event.getPassword();
-    this.active = true;
-  }*/
 }
