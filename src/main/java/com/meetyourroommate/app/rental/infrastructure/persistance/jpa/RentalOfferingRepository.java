1662631b54da48682dd2e39baae5edb4ce160ecc
XantX <hp_xantx@hotmail.com>
2022-10-23 23:21:47 -0500

    <feat>[offers]:List all offers by user id

--- src/main/java/com/meetyourroommate/app/rental/infrastructure/persistance/jpa/RentalOfferingRepository.java
+++ src/main/java/com/meetyourroommate/app/rental/infrastructure/persistance/jpa/RentalOfferingRepository.java
@@ -1,13 +1,17 @@
 package com.meetyourroommate.app.rental.infrastructure.persistance.jpa;
 
+import com.meetyourroommate.app.profile.domain.aggregates.Profile;
 import com.meetyourroommate.app.property.domain.aggregates.Property;
+import com.meetyourroommate.app.rental.domain.aggregates.Rental;
 import com.meetyourroommate.app.rental.domain.entities.RentalOffering;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RentalOfferingRepository extends JpaRepository<RentalOffering, Long> {
     Optional<RentalOffering> findByProperty(Property property);
+    List<RentalOffering> findAllByProperty_Profile(Profile profile);
 }
