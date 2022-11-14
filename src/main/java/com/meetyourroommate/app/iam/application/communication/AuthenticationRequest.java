abeabf62a570895ce4c083ec8d2ab85a7872c184
XantX <hp_xantx@hotmail.com>
2022-11-14 14:58:39 -0500

    <fix>[email]:update name of email property

--- src/main/java/com/meetyourroommate/app/iam/application/communication/AuthenticationRequest.java
+++ src/main/java/com/meetyourroommate/app/iam/application/communication/AuthenticationRequest.java
@@ -1,13 +1,8 @@
 package com.meetyourroommate.app.iam.application.communication;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import com.meetyourroommate.app.iam.domain.valueobjects.Email;
 import com.meetyourroommate.app.iam.domain.valueobjects.Password;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class AuthenticationRequest {
@@ -15,7 +10,7 @@ public class AuthenticationRequest {
   private Password password;
   public AuthenticationRequest (){}
   public AuthenticationRequest (String email, String password){
-    this.email = new Email().setAdress(email);
+    this.email = new Email().setAddress(email);
     this.password = new Password().setPassword(password);
   }
 
--- src/main/java/com/meetyourroommate/app/iam/domain/valueobjects/Email.java
+++ src/main/java/com/meetyourroommate/app/iam/domain/valueobjects/Email.java
@@ -2,18 +2,16 @@ package com.meetyourroommate.app.iam.domain.valueobjects;
 
 import javax.persistence.Embeddable;
 
-import lombok.*;
-
 @Embeddable
 public class Email {
-  private String adress;
+  private String address;
 
-  public String getAdress() {
-    return adress;
+  public String getAddress() {
+    return address;
   }
 
-  public Email setAdress(String adress) {
-    this.adress = adress;
+  public Email setAddress(String address) {
+    this.address = address;
     return this;
   }
 }
