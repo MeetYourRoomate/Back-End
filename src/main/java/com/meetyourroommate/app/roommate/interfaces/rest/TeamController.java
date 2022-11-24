14e759e5c0428204f8a50f0ca8ccc44d2f08f513
XantX <hp_xantx@hotmail.com>
2022-11-24 18:55:59 -0500

    <feat>[team]: Team disolve

--- src/main/java/com/meetyourroommate/app/roommate/interfaces/rest/TeamController.java
+++ src/main/java/com/meetyourroommate/app/roommate/interfaces/rest/TeamController.java
@@ -2,12 +2,15 @@ package com.meetyourroommate.app.roommate.interfaces.rest;
 
 import com.meetyourroommate.app.profile.application.services.ProfileService;
 import com.meetyourroommate.app.profile.domain.aggregates.Profile;
+import com.meetyourroommate.app.profile.domain.enumerate.TeamStatus;
 import com.meetyourroommate.app.roommate.application.communication.RoommateListDtoResponse;
 import com.meetyourroommate.app.roommate.application.communication.TeamListResponse;
 import com.meetyourroommate.app.roommate.application.communication.TeamResponse;
+import com.meetyourroommate.app.roommate.application.services.DutyService;
 import com.meetyourroommate.app.roommate.application.services.RoommateService;
 import com.meetyourroommate.app.roommate.application.services.TeamService;
 import com.meetyourroommate.app.roommate.application.tranform.RoommateDtoMapper;
+import com.meetyourroommate.app.roommate.domain.entities.Duty;
 import com.meetyourroommate.app.roommate.domain.entities.Roommate;
 import com.meetyourroommate.app.roommate.domain.entities.Team;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,10 +20,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,12 +30,14 @@ import java.util.Optional;
 public class TeamController {
     private TeamService teamService;
     private RoommateService roommateService;
+    private DutyService dutyService;
     private ProfileService profileService;
     private final RoommateDtoMapper roommateDtoMapper;
 
-    public TeamController(TeamService teamService, RoommateService roommateService, ProfileService profileService, RoommateDtoMapper roommateDtoMapper) {
+    public TeamController(TeamService teamService, RoommateService roommateService, DutyService dutyService, ProfileService profileService, RoommateDtoMapper roommateDtoMapper) {
         this.teamService = teamService;
         this.roommateService = roommateService;
+        this.dutyService = dutyService;
         this.profileService = profileService;
         this.roommateDtoMapper = roommateDtoMapper;
     }
@@ -143,6 +145,53 @@ public class TeamController {
                    new TeamResponse(e.getMessage()),
                    HttpStatus.INTERNAL_SERVER_ERROR
            );
+        }
+    }
+
+    @Operation(summary = "Delete team by id", description = "Delete team and give roommates equipment-free status", tags = {"Teams"})
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Team")
+    })
+    @DeleteMapping(value = "/teams/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TeamResponse> deleteTeamById(@PathVariable("id")Long teamId){
+        try{
+            Optional<Team> team = teamService.findById(teamId);
+            if(team.isEmpty()){
+                return new ResponseEntity<>(
+                        new TeamResponse("Team not found."),
+                        HttpStatus.NOT_FOUND
+                );
+            }
+            List<Roommate> roommates = team.get().getRoommates();
+            List<Duty> duties = team.get().getDuties();
+            duties.forEach(duty -> {
+                try {
+                    dutyService.deleteById(duty.getId());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            roommates.forEach((roommate) -> {
+                try {
+                    Profile roProfile = roommate.getProfile();
+                    roProfile.setTeamStatus(TeamStatus.WITHOUTTEAM);
+                    profileService.save(roProfile);
+                    roommateService.deleteById(roommate.getId());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            teamService.deleteById(teamId);
+            return new ResponseEntity<>(
+                    new TeamResponse("Equipment satisfactorily dissolved."),
+                    HttpStatus.OK
+            );
+        }catch (Exception e){
+            return new ResponseEntity<>(
+                    new TeamResponse(e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
